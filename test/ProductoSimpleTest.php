80d660473611d8c5c30d8308bbcbe7bdc7ef8679
YORK DAU <yorkdau@gmail.com>
2020-10-08 18:28:31 -0500

    8/10/2020/ casodePrueba1

--- test/ProductoSimpleTest.php
+++ test/ProductoSimpleTest.php
@@ -24,9 +24,32 @@ class ProductoSimpleTest extends TestCase
      * @test
      */
 
-    public function testCantidadEntradaNegativaCero ():void {
-        $producto = new ProductoSimple('gaseosa litro',5000,5000,2,'VENTA DIRECTA');
+    public function testCantidadEntradaNegativaCero(): void
+    {
+        $producto = new ProductoSimple('gaseosa litro', 5000, 5000, 2, 'VENTA DIRECTA');
         $resultado = $producto->Entrada(0);
-        $this->assertEquals('Cantidad de productos que intenta registrar es incorrecta, bebe ser mayor que cero',$resultado);
+        $this->assertEquals('Cantidad de productos que intenta registrar es incorrecta, bebe ser mayor que cero', $resultado);
+    }
+
+    /**
+     *
+     * Escenario: Registro del valor de la cantidad correcto en la entrada.
+     * HU 1. COMO USUARIO QUIERO REGISTRAR LA ENTRADA PRODUCTOS
+     * Criterio de Aceptación:
+     * 1. La cantidad de la entrada debe ser mayor a 0.
+     * 2. La cantidad de la entrada se le aumentará a la cantidad existente del producto
+     * Dado
+
+     * El usuario tiene un  un producto registrado con el: nombre:”gaseosa litro”, precio:”5000”, cantidad:”2”, costo:”5000”, tipo:”VENTA DIRECTA”.
+     * Cuando
+     * registre una nueva entrada al valor de la cantidad correcta es decir mayor a cero,cantidad = “7”
+     * Entonces
+     * El sistema de informacion registrara y mostrara un mensaje de exito como : “Se ha agregado 7 unidades al producto gaseosa litro en el stock hay:9";
+     * @test
+     */
+    public function testCantidadCorrectaEntrada():void {
+        $producto = new ProductoSimple('gaseosa litro', 5000, 5000, 2, 'VENTA DIRECTA');
+        $resultado = $producto->Entrada(7);
+        $this->assertEquals('Se ha agregado 7 unidades al producto gaseosa litro en el stock hay:9',$resultado);
     }
 }
